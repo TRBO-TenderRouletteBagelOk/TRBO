edd53b5e789265cb1904db010c4bf5cca96b8350
tyrese <tyrese225@hotmail.com>
2017-02-11 13:10:10 -0800

    inital

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// Static folder for css/js files. ( /assets/css , /assets/js)
+app.use(('/assets'), express.static('assets'));
+
+app.get('/', function(req,res) {
+   // Anh, load the home/index file here.
+}
+
+app.listen(3000);
+console.log('Server has begun.')
