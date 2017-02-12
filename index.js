5b5ee21ef133bbaf379662a07cd06f781b0c89bb
tyrese <tyrese225@hotmail.com>
2017-02-11 19:36:35 -0800

    feature: new css,templates,routes and foundation

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
+var exphbs  = require('express-handlebars');
 var app = express();
-// Mongoose = MongoDB
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://coolkid:cool123@ds147979.mlab.com:47979/trbo_users');
 // Data-parsing
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Mongoose Models
-var User = require('/models/user');
+app.post('/register', (req, res ) => {
+	const password = req.body.pword
+    , username = req.body.username
+	, gender = req.body.gender
+	, age = req.body.age
+	name = req.body.uname;
 
-app.post('/sendform', )
+	res.render('swipe');
+});
+//HandleBarsJS Templating
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 // Routing
+app.use(('/bower_components'), express.static('bower_components'));
 app.use(('/assets'), express.static('assets'));
 app.get('/', function(req,res) {
-  res.sendFile(__dirname+'/index.html');
-}
-app.listen(3000);
-console.log('Server has begun.')
+  res.render('home');
+});
+app.get('/swipe', function(req,res) {
+	res.render('swipe');
+});
+app.post('/sendfrom', function(req,res) {
+
+});
+
+ app.listen(8000, function () {
+ console.log("App now running on port 8000");
+});
--- models/user.js
+++ models/user.js
@@ -1,10 +1,13 @@
+
+var mongoose = require('mongoose');
+
 var userSchema = new Schema({
 	name: String,
 	username: String,
 	password: String,
 	gender: String,
 	age: String,
-	hobbies: Array
+	hobbies: Array,
     
 	// updated at:
 	updated_at: Date
