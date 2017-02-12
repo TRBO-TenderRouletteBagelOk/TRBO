091f4f924f9e3fc0a97ad15ef18ad0d36d53d66f
Wenzhao Liu <zmaxlwz@gmail.com>
2017-02-12 11:56:12 -0800

    modify index.js
    modify index.js to add video function

--- index.js
+++ index.js
@@ -49,6 +49,114 @@ app.post('/video2', function(req, res){
 	res.render('video2');
 });
 
+
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+
+// Connection URL
+var url = 'mongodb://localhost:27017/test';
+
+var updateDocument = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('chatusers');
+  // Update document where a is 2, set b equal to 1
+
+  var user_num_ret = 1;
+  var the_other_num = 0;
+
+  collection.find({}).toArray(function(err, docs) {
+    assert.equal(err, null);
+    console.log("Found the following records");
+    console.log(docs)
+
+    console.log(docs[0].user_num)
+    if(docs[0].user_num == 0){
+    	console.log("yes")
+    	//user_num_ret 
+    	user_num_ret = 0
+    	the_other_num = 1
+    }
+    else{
+    	console.log("no")
+    	user_num_ret = 1
+    	the_other_num = 0
+    }
+
+    //user_num_ret = docs[0].user_num
+    //callback(docs);
+
+    collection.updateOne({ user_num :  user_num_ret}
+    , { $set: { user_num : the_other_num } }, function(err, result) {
+    assert.equal(err, null);
+    assert.equal(1, result.result.n);
+    console.log("Updated the document with the field a equal to 2");
+    callback(result);
+  	}); 
+
+  });
+  
+}
+
+app.post('/video', function(req, res){
+	//res.render('video2');
+
+	MongoClient.connect(url, function(err, db) {
+  	assert.equal(null, err);
+  	console.log("Connected successfully to server");
+  	/*
+  	updateDocument(db, function() {
+      db.close();
+  	});
+	*/
+
+	// Get the documents collection
+  var collection = db.collection('chatusers');
+  // Update document where a is 2, set b equal to 1
+
+  var user_num_ret = 1;
+  var the_other_num = 0;
+
+  collection.find({}).toArray(function(err, docs) {
+    assert.equal(err, null);
+    console.log("Found the following records");
+    console.log(docs)
+
+    console.log(docs[0].user_num)
+    if(docs[0].user_num == 0){
+    	console.log("yes")
+    	//user_num_ret 
+    	res.render('video1');
+    	user_num_ret = 0
+    	the_other_num = 1
+    }
+    else{
+    	console.log("no")
+    	res.render('video2');
+    	user_num_ret = 1
+    	the_other_num = 0
+    }
+
+    //user_num_ret = docs[0].user_num
+    //callback(docs);
+
+    collection.updateOne({ user_num :  user_num_ret}
+    , { $set: { user_num : the_other_num } }, function(err, result) {
+    assert.equal(err, null);
+    assert.equal(1, result.result.n);
+    console.log("Updated the document with the field a equal to 2");
+    //callback(result);
+    db.close();
+  	}); 
+
+  });
+
+  	});
+
+  	//res.sendFile(__dirname+'/index.html');
+
+
+});
+
 app.get('/matching', function(req,res) {
 	res.render('matching', {layout: "foundation"});
 });
