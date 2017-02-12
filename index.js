44d9ee9a73c3c9b497331678ad393058df5f1e24
Anh Nguyen <anhhn1@gmail.com>
2017-02-11 19:59:17 -0800

    start simple form page

--- index.js
+++ index.js
@@ -8,7 +8,17 @@ app.use(('/assets'), express.static('assets'));
 app.use(express.static('public'))
 
 app.get('/', function(req,res) { // res = response, req = request
-   res.sendFile(__dirname+'/index.html');
+  res.sendFile(__dirname+'/index.html');
+});
+
+app.get('/simple-form', function (req, res) {
+  res.sendFile(__dirname+'/simple-form.html');
+});
+
+app.post('/submit-form', function (req, res) {
+  console.log(req.body.iama);
+  console.log(req.body.lookingFor);
+  res.sendFile(__dirname+'/simple-form.html');
 });
 
 app.listen(3000);
